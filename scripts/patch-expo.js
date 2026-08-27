5d5bf556bcbcad94cb8a949b17fa167d996a5a97
vnnit <vnnit@users.noreply.github.com>
2026-08-26 22:31:03 -0700

    fix_patch_expo_modules_jsi_runtime_scheduler_swift_6_2

---
+++ scripts/patch-expo.js
@@ -0,0 +1,20 @@
+﻿const fs = require('fs');
+const path = require('path');
+
+// 1. Patch RuntimeScheduler.h in expo-modules-jsi
+const targetHeader = path.join(__dirname, '../node_modules/expo-modules-jsi/apple/Sources/ExpoModulesJSI-Cxx/include/RuntimeScheduler.h');
+if (fs.existsSync(targetHeader)) {
+  let content = fs.readFileSync(targetHeader, 'utf8');
+  content = content.replace(/SWIFT_RETURNS_RETAINED\s+RuntimeScheduler/g, 'RuntimeScheduler');
+  fs.writeFileSync(targetHeader, content, 'utf8');
+  console.log('[Patch] Successfully patched RuntimeScheduler.h');
+}
+
+// 2. Patch Package.swift to swift-tools-version: 6.0
+const targetPackage = path.join(__dirname, '../node_modules/expo-modules-jsi/apple/Package.swift');
+if (fs.existsSync(targetPackage)) {
+  let content = fs.readFileSync(targetPackage, 'utf8');
+  content = content.replace('// swift-tools-version: 6.2', '// swift-tools-version: 6.0');
+  fs.writeFileSync(targetPackage, content, 'utf8');
+  console.log('[Patch] Successfully patched Package.swift');
+}
